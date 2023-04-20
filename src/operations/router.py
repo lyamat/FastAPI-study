4f9fb3c2893aa305ea849dbf58224a28c7cbc555
lyamat <ilyamatveev12902@gmail.com>
2023-04-21 02:12:22 +0300

    6 step

---
+++ src/operations/router.py
@@ -0,0 +1,28 @@
+from typing import List
+from fastapi import APIRouter, Depends
+from sqlalchemy import select, insert
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from database import get_async_session
+from operations.models import operation
+from operations.schemas import OperationCreate, Operation
+
+router = APIRouter(
+    prefix="/operations",
+    tags=["Operation"]
+)
+
+
+@router.get("/", response_model=List[Operation])
+async def get_specific_operations(operation_type: str, session: AsyncSession = Depends(get_async_session)):
+    query = select(operation).where(operation.c.type == operation_type)
+    result = await session.execute(query)
+    return result.all()
+
+
+@router.post("/")
+async def add_specific_operations(new_operation: OperationCreate, session: AsyncSession = Depends(get_async_session)):
+    stmt = insert(operation).values(**new_operation.dict())
+    await session.execute(stmt)
+    await session.commit()
+    return {"status": "success"}
---
+++ src/operations/schemas.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+
+from pydantic import BaseModel
+
+
+class OperationCreate(BaseModel):
+    id: int
+    quantity: str
+    figi: str
+    instrument_type: str
+    date: datetime
+    type: str
+
+class Operation(BaseModel):
+    id: int
+    quantity: str
+    figi: str
+    instrument_type: str
+    date: datetime
+    type: str
+    
+
+    class Config:
+        orm_mode = True
